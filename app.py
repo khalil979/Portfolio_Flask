d264407302262d435cc843eaabcc6877ea29331d
Khalil Ibrahim <100088472+khalil979@users.noreply.github.com>
2023-02-16 16:20:59 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, url_for, redirect
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+
+def index():
+
+  return render_template("index.html")
+
+
+
+
+
+if __name__ == "main":
+
+  app.run()
